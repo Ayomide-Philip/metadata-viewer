c0015236f3bf7534be5637e2d1bbb211baad0f08
Areo Ayomide <areoayomide2008@gmail.com>
2025-06-27 01:02:20 +0100

    Implement image upload functionality with preview in Home component

--- app/page.js
+++ app/page.js
@@ -1,31 +1,51 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [imageUrl, setImageUrl] = useState(null);
+  function imageReader(e) {
+    if (e.files && e.files[0]) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        console.log(event.target.result);
+        setImageUrl(event.target.result);
+      };
+      reader.readAsDataURL(e.files[0]);
+    }
+  }
   return (
-    <div className="py-20 bg-white px-2">
-      <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
-        <div className="md:flex">
-          <div className="w-full p-3">
-            <div className="relative h-48 rounded-lg border-dashed border-2 border-black bg-gray-100 flex justify-center items-center">
-              <div className="absolute">
-                <div className="flex flex-col items-center">
-                  <i className="fa fa-folder-open fa-4x text-blue-700"></i>
-                  <span className="block text-black font-normal">
-                    Upload your Image Here
-                  </span>
+    <>
+      <div className="py-20 px-2">
+        <div className="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-xl">
+          <div className="md:flex">
+            <div className="w-full p-3">
+              <div className="relative h-48 rounded-lg border-dashed border-2 border-black bg-gray-100 flex justify-center items-center">
+                <div className="absolute">
+                  <div className="flex flex-col items-center">
+                    <i className="fa fa-folder-open fa-4x text-blue-700"></i>
+                    <span className="block text-black font-normal">
+                      Upload your Image Here
+                    </span>
+                  </div>
                 </div>
-              </div>
 
-              <input
-                type="file"
-                className="h-full w-full opacity-0"
-                name="photo"
-                accept="image/*"
-              />
+                <input
+                  type="file"
+                  className="h-full w-full opacity-0"
+                  name="photo"
+                  accept="image/*"
+                  onChange={(e) => {
+                    imageReader(e.target);
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div>
+        <img src={imageUrl} height="300px" />
+      </div>
+    </>
   );
 }
